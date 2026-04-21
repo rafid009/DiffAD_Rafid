b80ff2b9f8f275529ec177f45faa885bb8f75a82
rafid009 <rfd.009@gmail.com>
2026-04-21 08:50:29 -0700

    mormalizing code added 1-4

--- normalize.py
+++ normalize.py
@@ -17,12 +17,12 @@ swat_train = (swat_train - swat_mean) / swat_std
 swat_train.to_csv(f"{swat_folder}/swat_train.csv", index=False)
 swat_test = (swat_test - swat_mean) / swat_std
 swat_test.to_csv(f"{swat_folder}/swat_test.csv", index=False)
-np.save(f"{swat_folder}/swat_mean.npy", swat_mean.to_numpy())
-np.save(f"{swat_folder}/swat_std.npy", swat_std.to_numpy())
+np.save(f"{swat_folder}/swat_mean.npy", swat_mean)
+np.save(f"{swat_folder}/swat_std.npy", swat_std)
 synth_train = (synth_train - synth_mean) / synth_std
 synth_train.to_csv(f"{synth_folder}/synth_train.csv", index=False)
 synth_test = (synth_test - synth_mean) / synth_std
 synth_test.to_csv(f"{synth_folder}/synth_test.csv", index=False)
-np.save(f"{synth_folder}/synth_mean.npy", synth_mean.to_numpy())
-np.save(f"{synth_folder}/synth_std.npy", synth_std.to_numpy())
+np.save(f"{synth_folder}/synth_mean.npy", synth_mean)
+np.save(f"{synth_folder}/synth_std.npy", synth_std)
 
